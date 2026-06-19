ebbada66e83e6fe37a9590ae13ab8502ff4ceed7
Anshul-temp <anshulyadav191006@gmail.com>
2026-06-19 15:22:14 +0530

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define F first
+#define S second
+
+using ll = long long;
+using vi = vector<int>;
+using pii = pair<int,int>;
+
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+// const int MOD = 998244353;
+
+void solve() {
+
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
